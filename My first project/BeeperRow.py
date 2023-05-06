0d029aa7372b33199a3de5a7c8976afc653867d7
JohnnyTsao <131897250+JohnnyTsao@users.noreply.github.com>
2023-05-07 01:57:56 +0800

    Update BeeperRow.py

--- My first project/BeeperRow.py
+++ My first project/BeeperRow.py
@@ -1,6 +1,6 @@
 """
 File: BeeperRow.py
-Name:
+Name:Johnny Tsao
 -------------------------
 This program makes Karel fill up
 Street 1 with beepers
@@ -11,7 +11,10 @@ from karel.stanfordkarel import *
 
 
 def main():
-    pass
+    while front_is_clear():
+        put_beeper()
+        move()
+    put_beeper()
 
 
 # ----- DO NOT MODIFY CODE BELOW THIS LINE ----- #
