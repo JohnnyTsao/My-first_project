552fbf491522b3dad920b296bd81d6e993664080
JohnnyTsao <131897250+JohnnyTsao@users.noreply.github.com>
2023-05-07 02:06:16 +0800

    Update DoubleBeepers.py

--- My first project/DoubleBeepers.py
+++ My first project/DoubleBeepers.py
@@ -1,6 +1,6 @@
 """
 File: DoubleBeepers.py
-Name:
+Name:Johnny Tsao
 -------------------------------
 TODO:
 """
@@ -12,7 +12,46 @@ def main():
     """
     Karel will double the beepers
     """
-    pass
+    move()
+    double_beepers()
+    put_beepers_back()
+    karel_goes_home()
+
+def double_beepers():
+    while on_beeper():
+        #old beepers,facing East.
+        pick_beeper()
+        move()
+        put_beeper()
+        put_beeper()
+        #new beepers,facing East.
+        turn_around()
+        move()
+        #old beepers,facing West.
+        turn_around()
+
+
+def put_beepers_back():
+    move()
+    while on_beeper():
+        pick_beeper()
+        turn_around()
+        move()
+        put_beeper()
+        turn_around()
+        move()
+
+
+def karel_goes_home():
+    turn_around()
+    move()
+    move()
+    turn_around()
+
+
+def turn_around():
+    turn_left()
+    turn_left()
 
 
 # ----- DO NOT MODIFY CODE BELOW THIS LINE ----- #
